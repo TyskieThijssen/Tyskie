1be5022db2eb81dec408aedefd55171633aa4fac
¨tyskiethijssen¨ <thijs.thijssen@hotmail.com>
2017-02-21 17:32:34 +0100

    dit is een commit

--- GitHubTestProject/src/test/java/oose/dea/TestTest.java
+++ GitHubTestProject/src/test/java/oose/dea/TestTest.java
@@ -1,10 +1,16 @@
 package oose.dea;
 
+import org.junit.*;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by tyskie on 2/18/17.
  */
 public class TestTest {
+    @org.junit.Test
+    public void testTest(){
+        Test sut = new Test();
 
+    }
 }
